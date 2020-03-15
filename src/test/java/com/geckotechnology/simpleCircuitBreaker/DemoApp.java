fd902da72d5f6bb4cbb4d5c55fbfbf511b2e253d
guyplusplus <>
2020-03-16 06:41:35 +0700

    fixed variable name

--- src/test/java/com/geckotechnology/simpleCircuitBreaker/DemoApp.java
+++ src/test/java/com/geckotechnology/simpleCircuitBreaker/DemoApp.java
@@ -8,8 +8,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class DemoApp {
 	
 	private static final Random rd = new Random(System.currentTimeMillis());
+	private static final String CONFIG_RESOURCE = "/demo.config";
 	private static final int NB_THREADS = 5;
-	private static final float ACTUAL_FAILURE_RATIO_RANGE_PCT = 70;
+	private static final float ACTUAL_SUCCESS_RATE_RANGE_PCT = 70;
 	private static final long ACTUAL_CALL_DURATION_RANGE_MS = 1500;
 	private static final long WAIT_BETWEEN_LOOP_RANGE_MS = 1000;
 
@@ -26,9 +27,10 @@ public class DemoApp {
 	}
 	
 	private void loadConfigFile() {
+		//load configuration from resource
 		try {
 			Properties props = new Properties();
-			InputStream is = CircuitBreakerConfigTest.class.getResourceAsStream("/demo.config");
+			InputStream is = CircuitBreakerConfigTest.class.getResourceAsStream(CONFIG_RESOURCE);
 			props.load(is);
 			is.close();
 			CircuitBreakerConfig config = new CircuitBreakerConfig(props);
@@ -58,7 +60,7 @@ public class DemoApp {
 					long duration;
 					while(true) {
 						if(circuitBreaker.isClosedForThisCall()) {
-							boolean willFail = (rd.nextInt(1000) > 10 * ACTUAL_FAILURE_RATIO_RANGE_PCT);
+							boolean willFail = (rd.nextInt(1000) > 10 * ACTUAL_SUCCESS_RATE_RANGE_PCT);
 							duration = rd.nextInt((int)ACTUAL_CALL_DURATION_RANGE_MS); 
 							if(willFail) {
 								System.out.println("[" + threadId.get() + "] Breaker is closed. Simulating callFailed with duration: " + duration);
